84e3db7561a0edbd97d4d8079151b0f3c20ef2d1
javierlnmn <jlm03123@gmail.com>
2026-06-11 18:35:17 +0200

    Re-implement file utils

---
+++ utils/files.py
@@ -0,0 +1,16 @@
+import json
+from pathlib import Path
+
+
+def read_file_data(file_path: str) -> str:
+    return Path(file_path).read_text()
+
+
+def read_file_data_json(file_path: str) -> str:
+    return json.loads(read_file_data(file_path))
+
+
+def write_file_data(file_path: str, data: str) -> None:
+    path = Path(file_path)
+    path.parent.mkdir(parents=True, exist_ok=True)
+    path.write_text(data)
