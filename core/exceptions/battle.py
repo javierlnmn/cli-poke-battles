81ee915efb4e73659f2750e839f0e3c5927fb969
javierlnmn <jlm03123@gmail.com>
2026-06-16 19:51:06 +0200

    Solve circular import in illegal battle move exception

--- core/exceptions/battle.py
+++ core/exceptions/battle.py
@@ -1,10 +1,7 @@
-from core.entities import BattlePokemonMove
-
-
 class IllegalBattleMoveError(Exception):
-    def __init__(self, battle_move: BattlePokemonMove) -> None:
-        super().__init__(
-            f"""Illegal battle move '{battle_move.move.name}'.
-            Current move status: {battle_move.current_pp}/{battle_move.move.pp}"""
-        )
-        self.battle_move = battle_move
+    def __init__(self, pokemon_name: str, move_name: str, current_pp: int, max_pp: int) -> None:
+        super().__init__(f"Illegal battle move '{move_name}' for {pokemon_name} (PP: {current_pp}/{max_pp}).")
+        self.pokemon_name = pokemon_name
+        self.move_name = move_name
+        self.current_pp = current_pp
+        self.max_pp = max_pp
