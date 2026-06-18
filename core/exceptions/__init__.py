e644faf345d1f3aa0bd4b5e4cd28117e14db5e7b
javierlnmn <jlm03123@gmail.com>
2026-06-18 08:25:21 +0200

    Refactor exceptions and add repository resource not found

--- core/exceptions/__init__.py
+++ core/exceptions/__init__.py
@@ -1,5 +1,7 @@
 from core.exceptions.battle import IllegalBattleMoveError
+from core.exceptions.repositories import ResourceNotFoundError
 
 __all__ = [
     "IllegalBattleMoveError",
+    "ResourceNotFoundError",
 ]
--- core/exceptions/battle.py
+++ core/exceptions/battle.py
@@ -1,7 +1,2 @@
 class IllegalBattleMoveError(Exception):
-    def __init__(self, pokemon_name: str, move_name: str, current_pp: int, max_pp: int) -> None:
-        super().__init__(f"Illegal battle move '{move_name}' for {pokemon_name} (PP: {current_pp}/{max_pp}).")
-        self.pokemon_name = pokemon_name
-        self.move_name = move_name
-        self.current_pp = current_pp
-        self.max_pp = max_pp
+    pass
