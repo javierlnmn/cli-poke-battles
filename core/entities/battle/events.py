44eb3ad56cc8a6dcb326b6249bdd0d44873ab871
javierlnmn <jlm03123@gmail.com>
2026-06-26 13:04:30 +0200

    Implement state manager methods

--- core/entities/battle/events.py
+++ core/entities/battle/events.py
@@ -8,22 +8,38 @@ from core.entities.pokemon import StatEnum
 
 @dataclass(frozen=True)
 class BattleEventMoveUsed:
+    battle_pokemon: BattlePokemon
     battle_move: BattlePokemonMove
 
 
 @dataclass(frozen=True)
 class BattleEventDamage:
+    battle_pokemon: BattlePokemon
     damage_dealt_hp: int
 
 
 @dataclass(frozen=True)
+class BattleEventHeal:
+    battle_pokemon: BattlePokemon
+    healing_hp: int
+
+
+@dataclass(frozen=True)
 class BattleEventNotAffected:
+    attacker: BattlePokemon
+    target: BattlePokemon
     battle_pokemon: BattlePokemon
 
 
 @dataclass(frozen=True)
 class BattleEventMissed:
-    pass
+    attacker: BattlePokemon
+    battle_move: BattlePokemonMove
+
+
+@dataclass(frozen=True)
+class BattleEventDodged:
+    target: BattlePokemon
 
 
 @dataclass(frozen=True)
@@ -33,30 +49,59 @@ class BattleEventFainted:
 
 @dataclass(frozen=True)
 class BattleEventStatChange:
+    battle_pokemon: BattlePokemon
     stat: StatEnum
     amount: int
 
 
 @dataclass(frozen=True)
+class BattleEventStatsRestore:
+    battle_pokemon: BattlePokemon
+
+
+@dataclass(frozen=True)
+class BattleEventAllStatsRestore:
+    battle_pokemon: BattlePokemon
+
+
+@dataclass(frozen=True)
 class BattleEventAilment:
+    battle_pokemon: BattlePokemon
     ailment: AilmentEnum
+
+
+@dataclass(frozen=True)
+class BattleEventAilmentRestored:
+    battle_pokemon: BattlePokemon
+    ailment: AilmentEnum
+
+
+@dataclass(frozen=True)
+class BattleEventAllAilmentsRestored:
+    battle_pokemon: BattlePokemon
+    prev_ailments: list[AilmentEnum]
 
 
 BattleEventKind = (
     BattleEventMoveUsed
     | BattleEventDamage
+    | BattleEventHeal
     | BattleEventNotAffected
     | BattleEventMissed
+    | BattleEventDodged
     | BattleEventFainted
     | BattleEventStatChange
+    | BattleEventStatsRestore
+    | BattleEventAllStatsRestore
+    | BattleEventStatChange
     | BattleEventAilment
+    | BattleEventAilmentRestored
+    | BattleEventAllAilmentsRestored
 )
 
 
 @dataclass(frozen=True)
 class BattleEvent:
     kind: type[BattleEventKind]
-    actor: BattlePokemon
     payload: BattleEventKind
     text_detail: str | None = None
-    target: BattlePokemon | None = None
--- core/entities/battle/state_manager.py
+++ core/entities/battle/state_manager.py
@@ -1,10 +1,21 @@
 from dataclasses import dataclass, field
 
-from core.entities.battle.events import BattleEvent, BattleEventDamage, BattleEventMoveUsed
+from core.entities.battle.events import (
+    BattleEvent,
+    BattleEventAilment,
+    BattleEventAilmentRestored,
+    BattleEventAllAilmentsRestored,
+    BattleEventAllStatsRestore,
+    BattleEventDamage,
+    BattleEventHeal,
+    BattleEventMoveUsed,
+    BattleEventStatChange,
+    BattleEventStatsRestore,
+)
 from core.entities.battle.move import BattlePokemonMove
 from core.entities.battle.pokemon import BattlePokemon
-from core.entities.moves import AilmentEnum
-from core.entities.pokemon import PokemonStats
+from core.entities.moves import MAJOR_AILMENTS, AilmentEnum, MoveStatChange
+from core.exceptions.battle import MajorAilmentAlreadyActiveError
 
 
 @dataclass(frozen=True)
@@ -18,40 +29,144 @@ class BattleStateManager:
     current_turn: list[BattleEvent] = field(default_factory=list)
     turns_history: list[TurnResult] = field(default_factory=list)
 
-    def register_used_move(self, battle_move: BattlePokemonMove, actor: BattlePokemon):
+    def register_used_move(self, battle_move: BattlePokemonMove, battle_pokemon: BattlePokemon):
         self.current_turn.append(
             BattleEvent(
                 kind=BattleEventMoveUsed,
-                actor=actor,
-                text_detail=f"{actor.pokemon.name} used {battle_move.move.name}!",
-                payload=BattleEventMoveUsed(battle_move=battle_move),
+                text_detail=f"{battle_pokemon.pokemon.name} used {battle_move.move.name}!",
+                payload=BattleEventMoveUsed(
+                    battle_pokemon=battle_pokemon,
+                    battle_move=battle_move,
+                ),
             )
         )
 
     def apply_damage(self, battle_pokemon: BattlePokemon, health_points: int):
-        battle_pokemon.current_hp -= health_points
+        battle_pokemon.current_stats.hp -= health_points
         self.current_turn.append(
             BattleEvent(
                 kind=BattleEventDamage,
-                actor=battle_pokemon,
-                payload=BattleEventDamage(damage_dealt_hp=health_points),
+                payload=BattleEventDamage(
+                    battle_pokemon=battle_pokemon,
+                    damage_dealt_hp=health_points,
+                ),
             )
         )
 
     def apply_heal(self, battle_pokemon: BattlePokemon, health_points: int):
-        pass
+        battle_pokemon.current_stats.hp += health_points
+        self.current_turn.append(
+            BattleEvent(
+                kind=BattleEventHeal,
+                text_detail=f"{battle_pokemon.pokemon.name} restored {health_points} health points!",
+                payload=BattleEventHeal(
+                    battle_pokemon=battle_pokemon,
+                    healing_hp=health_points,
+                ),
+            )
+        )
 
     def apply_ailment(self, battle_pokemon: BattlePokemon, ailment: AilmentEnum):
-        pass
+        if ailment in MAJOR_AILMENTS and any(
+            ail in MAJOR_AILMENTS for ail in battle_pokemon.current_ailments
+        ):
+            raise MajorAilmentAlreadyActiveError(ailment.value)
+
+        battle_pokemon.current_ailments.append(ailment)
+        self.current_turn.append(
+            BattleEvent(
+                kind=BattleEventAilment,
+                # TODO: Create label for ailment
+                text_detail=f"{battle_pokemon.pokemon.name} is now {ailment.value}!",
+                payload=BattleEventAilment(
+                    battle_pokemon=battle_pokemon,
+                    ailment=ailment,
+                ),
+            )
+        )
 
     def restore_ailment(self, battle_pokemon: BattlePokemon, ailment: AilmentEnum):
-        pass
+        try:
+            battle_pokemon.current_ailments.remove(ailment)
+        except ValueError:
+            return
 
-    def apply_stats_change(self, battle_pokemon: BattlePokemon, new_stats: PokemonStats):
-        pass
+        self.current_turn.append(
+            BattleEvent(
+                kind=BattleEventAilmentRestored,
+                # TODO: Create label for ailment
+                text_detail=f"{battle_pokemon.pokemon.name} is not {ailment.value} anymore!",
+                payload=BattleEventAilmentRestored(
+                    battle_pokemon=battle_pokemon,
+                    ailment=ailment,
+                ),
+            )
+        )
+
+    def restore_all_ailments(self, battle_pokemon: BattlePokemon):
+        prev_ailments = battle_pokemon.current_ailments
+        battle_pokemon.current_ailments = []
+
+        self.current_turn.append(
+            BattleEvent(
+                kind=BattleEventAilmentRestored,
+                # TODO: Create label for ailment
+                text_detail=f"{battle_pokemon.pokemon.name} restored all its ailments!",
+                payload=BattleEventAllAilmentsRestored(
+                    battle_pokemon=battle_pokemon,
+                    prev_ailments=prev_ailments,
+                ),
+            )
+        )
+
+    def apply_stat_change(self, battle_pokemon: BattlePokemon, stat_change: MoveStatChange):
+        new_value = battle_pokemon.current_stats.get_stat(stat_change.stat) + stat_change.change
+        battle_pokemon.current_stats.set_stat(stat_change.stat, new_value)
+
+        verb = "rose" if stat_change.change > 0 else "fell"
+        stat_label = stat_change.stat.value.lower().replace("-", " ")
+        self.current_turn.append(
+            BattleEvent(
+                kind=BattleEventStatChange,
+                text_detail=(f"{battle_pokemon.pokemon.name}'s {stat_label} {verb}!"),
+                payload=BattleEventStatChange(
+                    battle_pokemon=battle_pokemon,
+                    stat=stat_change.stat,
+                    amount=stat_change.change,
+                ),
+            )
+        )
 
     def restore_stats(self, battle_pokemon: BattlePokemon):
-        pass
+        original_stats_except_hp = battle_pokemon.pokemon.stats
+        original_stats_except_hp.hp = battle_pokemon.current_stats.hp
+        battle_pokemon.current_stats = original_stats_except_hp
+
+        self.current_turn.append(
+            BattleEvent(
+                kind=BattleEventStatChange,
+                text_detail=(f"{battle_pokemon.pokemon.name}'s stats were restored!"),
+                payload=BattleEventStatsRestore(
+                    battle_pokemon=battle_pokemon,
+                ),
+            )
+        )
+
+    def restore_all_stats(self, battle_pokemon: BattlePokemon):
+        """
+        Same as restore stats, but also includes HP
+        """
+        battle_pokemon.current_stats = battle_pokemon.pokemon.stats
+
+        self.current_turn.append(
+            BattleEvent(
+                kind=BattleEventStatChange,
+                text_detail=(f"All of {battle_pokemon.pokemon.name} stats were restored!"),
+                payload=BattleEventAllStatsRestore(
+                    battle_pokemon=battle_pokemon,
+                ),
+            )
+        )
 
     def complete_turn(self):
         self.turns_history.append(self.current_turn)
